60fbe0dda4847c09d4ccdbaeb75e8c92248c8adf
Jim Latsko <slats2003@gmail.com>
2023-08-12 09:58:40 -0600

    some testing updates

--- src/main/java/com/example/fips/tomcat/CustomEmbeddedConnector.java
+++ src/main/java/com/example/fips/tomcat/CustomEmbeddedConnector.java
@@ -41,14 +41,14 @@ public class CustomEmbeddedConnector implements
                 connector.setProperty("sslHostConfigCertificateVerification", "optional");
                 connector.setProperty("sslHostConfigTruststoreFile", "conf/tomcat-fips.bcks");
                 connector.setProperty("sslHostConfigTruststorePassword", "This4Now");
-                connector.setProperty("sslHostConfigTruststoreType", "BCF-KS");   // REVISIT:  is this correct?
-                connector.setProperty("sslHostConfigTruststoreProvider", "BC FIPS");  // REVISIT:  is this correct?
+                connector.setProperty("sslHostConfigTruststoreType", "BCFKS");   // REVISIT:  is this correct?
+                connector.setProperty("sslHostConfigTruststoreProvider", "BCFIPS");  // REVISIT:  is this correct?
 
                 // set Certificate attributes in SSLHostConfig
                 connector.setProperty("sslHostConfigCertificateKeystoreFile", "conf/tomcat-fips.bcks");
                 connector.setProperty("sslHostConfigCertificateKeystorePassword", "This4Now");
-                connector.setProperty("sslHostConfigCertificateKeystoreType", "BCF-KS");  // REVISIT:  is this correct?
-                connector.setProperty("sslHostConfigCertificateKeystoreProvider", "BC FIPS");   // REVISIT:  is this correct?
+                connector.setProperty("sslHostConfigCertificateKeystoreType", "BCFKS");  // REVISIT:  is this correct?
+                connector.setProperty("sslHostConfigCertificateKeystoreProvider", "BCFIPS");   // REVISIT:  is this correct?
             }
         });
     }
